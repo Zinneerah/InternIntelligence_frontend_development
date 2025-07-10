bf92054c4b3b5d738d12048a4bae44a642f854c0
Zinneerah <138329715+Zinneerah@users.noreply.github.com>
2025-07-11 00:22:20 +0500

    Update firebaseauth.js

--- frontend_project/firebaseauth.js
+++ frontend_project/firebaseauth.js
@@ -1,18 +1,18 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-import {getAuth, createUserWithEmailAndPassword, LoginWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js"
-import {getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js"
+import {getAuth, createUserWithEmailAndPassword, LoginWithEmailAndPassword} from "firebase.js"
+import {getFirestore, setDoc, doc} from "firebase.js"
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
   const firebaseConfig = {
-    apiKey: "AIzaSyCqUwDuUoBpsJigFmaTur2WdRsOsuMkNFc",
-    authDomain: "login-project-bc694.firebaseapp.com",
-    projectId: "login-project-bc694",
-    storageBucket: "login-project-bc694.firebasestorage.app",
-    messagingSenderId: "323110630115",
-    appId: "1:323110630115:web:c385bc8f2852dec64f0bd6"
-  };
+  apiKey: "api key ",
+  authDomain: "project",
+  projectId: "projectid",
+  storageBucket: "bucket goes here",
+  messagingSenderId: "sender id",
+  appId: "api key"
+};
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
